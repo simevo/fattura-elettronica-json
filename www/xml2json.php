37ce07ce01e4b38f0fb14f729cd97953787a596d
Paolo Greppi <paolo.greppi@libpf.com>
2018-07-03 16:52:53 +0200

    WIP #7
    - protect against large XML files
      use the same size limit (5 MB) as the SdiCoop service (Regole Tecniche, 3.1.2)

--- www/xml2json.php
+++ www/xml2json.php
@@ -4,6 +4,13 @@
 
 declare(strict_types=1);
 
+// Check file size
+if ($_FILES["xml"]["size"] > 5*1024*1024) {
+    echo "Sorry, your XML file is too large.";
+    http_response_code(400);
+    die;
+}
+
 require("Xml2Json.php");
 
 $obj = new Simevo\Xml2Json($_FILES['xml']['tmp_name']);
